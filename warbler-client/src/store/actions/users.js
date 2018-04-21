dbc5d8b56cf30f485aea73fe56d7471c2ae03cf7
Rob Tucker <robt9095@gmail.com>
2018-04-21 08:47:30 -0700

    Add files via upload

--- warbler-client/src/store/actions/users.js
+++ warbler-client/src/store/actions/users.js
@@ -1,18 +1,24 @@
 import { apiCall } from '../../services/api';
 import { addError } from './errors';
-import { LOAD_USER } from '../actionTypes';
+import { LOAD_USER, UPDATE_CURRENT_USER } from '../actionTypes';
 
 export const loadUser = userData => ({
   type: LOAD_USER,
   userData
 });
 
-export function fetchUserData(username) {
+export const updateCurrentUser = user => ({
+  type: UPDATE_CURRENT_USER,
+  user
+});
+
+export function fetchUserData(username, currentUser) {
 	return dispatch => {
 		return new Promise((resolve,reject) => {
 			return apiCall('get', `/api/users/${username}`)
 			.then((res) => {
 			  dispatch(loadUser(res));
+        currentUser ? dispatch(updateCurrentUser(res)) : null;
 			  resolve();
 			})
 			.catch(err => {
