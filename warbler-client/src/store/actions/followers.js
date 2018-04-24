9f437c7ff10eaf3c7fa70fa90babfb83dbec83a9
Rob Tucker <robt9095@gmail.com>
2018-04-24 10:26:20 -0700

    refactors to Redux state

--- warbler-client/src/store/actions/followers.js
+++ warbler-client/src/store/actions/followers.js
@@ -1,58 +1,33 @@
 import { apiCall } from '../../services/api';
 import {addError} from './errors';
-import { ADD_FOLLOWER, REMOVE_FOLLOWER, LOAD_FOLLOWERS, LOAD_FOLLOWING } from '../actionTypes';
+import { ADD_FOLLOWING, REMOVE_FOLLOWING, USER_FOLLOWED, USER_UNFOLLOWED } from '../actionTypes';
 
-export const loadFollowers = followers => ({
-  type: LOAD_FOLLOWERS,
-  followers
+export const addFollowing = (userFollowed_id) => ({
+  type: ADD_FOLLOWING,
+  userFollowed_id
 });
 
-export const loadFollowing = following => ({
-  type: LOAD_FOLLOWING,
-  following
-});
-
-export const addFollower = (userFollowed_id, currentUser_id) => ({
-  type: ADD_FOLLOWER,
-  userFollowed_id,
+export const addFollower = (currentUser_id) => ({
+  type: USER_FOLLOWED,
   currentUser_id
 });
 
-export const removeFollower = (userFollowed_id, currentUser_id) => ({
-  type: REMOVE_FOLLOWER,
-  userFollowed_id,
-  currentUser_id
+export const removeFollowing = (userFollowed_id) => ({
+  type: REMOVE_FOLLOWING,
+  userFollowed_id
 });
 
-export const fetchFollowers = (user_id) => {
-  return dispatch => {
-    return apiCall('get', `/api/users/${user_id}/followers`)
-    .then((res) => {
-      dispatch(loadFollowers(res));
-    })
-    .catch(err => {
-      dispatch(addError(err.message));
-    });
-  };
-};
-
-export const fetchFollowing = (user_id) => {
-  return dispatch => {
-    return apiCall('get', `/api/users/${user_id}/following`)
-    .then((res) => {
-      dispatch(loadFollowing(res));
-    })
-    .catch(err => {
-      dispatch(addError(err.message));
-    });
-  };
-};
+export const removeFollower = (currentUser_id) => ({
+  type: USER_UNFOLLOWED,
+  currentUser_id
+});
 
 export const followUser = (userFollowed_id, currentUser_id) => {
 	return dispatch => {
 		return apiCall('post', `/api/users/${currentUser_id}/followers/${userFollowed_id}`)
 			.then(res => {
-				dispatch(addFollower(userFollowed_id, currentUser_id));
+				dispatch(addFollowing(userFollowed_id));
+				dispatch(addFollower(currentUser_id));
 			})
 			.catch(err => {
 				dispatch(addError(err.message));
@@ -64,7 +39,8 @@ export const unFollowUser = (userFollowed_id, currentUser_id) => {
 	return dispatch => {
 		return apiCall('delete', `/api/users/${currentUser_id}/followers/${userFollowed_id}`)
 			.then(res => {
-				dispatch(removeFollower(userFollowed_id, currentUser_id));
+				dispatch(removeFollowing(userFollowed_id));
+				dispatch(removeFollower(currentUser_id));
 			})
 			.catch(err => {
 				dispatch(addError(err.message));
