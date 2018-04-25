93ff372e66f3ad71c2bfa152a90e6113da67e8f9
Rob Tucker <robt9095@gmail.com>
2018-04-24 20:32:48 -0700

    Delete error.js

--- warbler-client/public/handlers/error.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(error, request, response, next) {
-	return response.status(error.status || 500).json({
-		error: {
-			message: error.message || 'Opps something went wrong.'
-		}
-	});
-}
-module.exports = errorHandler;
